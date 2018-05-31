5e9daf3f06b62ee856b298a79d111f22500e461c
Perelandra <luvyum4e@gmail.com>
2018-05-31 10:18:08 +0900

    프로젝트 생성

---
+++ sample-counter/src/main/java/com/perelandra/reactorviewmodel/sample/counter/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.perelandra.reactorviewmodel.sample.counter.ui.main
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.perelandra.reactorviewmodel.sample.counter.R
+
+class MainFragment : Fragment() {
+
+  companion object {
+    fun newInstance() = MainFragment()
+  }
+
+  private lateinit var viewModel: MainViewModel
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View {
+    return inflater.inflate(R.layout.main_fragment, container, false)
+  }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    // TODO: Use the ViewModel
+  }
+
+}
---
+++ sample-counter/src/main/java/com/perelandra/reactorviewmodel/sample/counter/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.perelandra.reactorviewmodel.sample.counter.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+  // TODO: Implement the ViewModel
+}
