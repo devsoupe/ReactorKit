3816b002c49bd0cc0ade5a91db2ea140bb6f48dc
Perelandra <luvyum4e@gmail.com>
2018-09-07 16:27:23 +0900

    Toolbar 추가

--- sample-githubsearch/src/main/java/com/perelandra/sample/githubsearch/ui/main/GithubSearchFragment.kt
+++ sample-githubsearch/src/main/java/com/perelandra/sample/githubsearch/ui/main/GithubSearchFragment.kt
@@ -2,12 +2,14 @@ package com.perelandra.sample.githubsearch.ui.main
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.buxikorea.buxi.library.reactorkit.ReactorView
 import com.perelandra.reactorviewmodel.extension.of
 import com.perelandra.sample.githubsearch.R
+import kotlinx.android.synthetic.main.github_search_fragment.*
 
 class GithubSearchFragment : Fragment(), ReactorView<GithubSearchViewModel> {
 
@@ -17,6 +19,12 @@ class GithubSearchFragment : Fragment(), ReactorView<GithubSearchViewModel> {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
     return inflater.inflate(R.layout.github_search_fragment, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    (activity as AppCompatActivity).setSupportActionBar(toolbar)
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -38,4 +46,5 @@ class GithubSearchFragment : Fragment(), ReactorView<GithubSearchViewModel> {
   override fun bindStates(viewModel: GithubSearchViewModel) {
 
   }
+
 }
