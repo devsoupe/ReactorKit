4341c61662511bb2a318d7ae27f8ffcfd2761f1f
perelandrax <luvyum4e@gmail.com>
2018-09-10 19:05:39 +0900

    GithubSearch 화면에 Toolbar 색상 적용 및 서치뷰 적용

--- sample-githubsearch/src/main/java/com/perelandra/sample/githubsearch/ui/main/GithubSearchFragment.kt
+++ sample-githubsearch/src/main/java/com/perelandra/sample/githubsearch/ui/main/GithubSearchFragment.kt
@@ -3,13 +3,13 @@ package com.perelandra.sample.githubsearch.ui.main
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.SearchView
+import android.view.*
 import com.buxikorea.buxi.library.reactorkit.ReactorView
 import com.perelandra.reactorviewmodel.extension.of
 import com.perelandra.sample.githubsearch.R
-import kotlinx.android.synthetic.main.github_search_fragment.*
+import kotlinx.android.synthetic.main.fragment_github_search.*
 
 class GithubSearchFragment : Fragment(), ReactorView<GithubSearchViewModel> {
 
@@ -18,13 +18,18 @@ class GithubSearchFragment : Fragment(), ReactorView<GithubSearchViewModel> {
   }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-    return inflater.inflate(R.layout.github_search_fragment, container, false)
+    return inflater.inflate(R.layout.fragment_github_search, container, false)
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
     (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+    list.layoutManager = LinearLayoutManager(context)
+    list.adapter = GithubSearchListAdapter()
+
+    setHasOptionsMenu(true)
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -35,6 +40,25 @@ class GithubSearchFragment : Fragment(), ReactorView<GithubSearchViewModel> {
   override fun onDestroyView() {
     super.onDestroyView()
     detachViewModel()
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+    inflater?.inflate(R.menu.menu_toolbar, menu)
+
+    val searchItem = menu?.findItem(R.id.action_search)
+    val searchView = searchItem?.actionView as SearchView
+
+    searchView.queryHint = "Search"
+    searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+      override fun onQueryTextSubmit(query: String?): Boolean = false
+
+      override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+      }
+    })
+
+    super.onCreateOptionsMenu(menu, inflater)
   }
 
   override fun createViewModel(): GithubSearchViewModel = GithubSearchViewModel().of(this)
