fe6e56e33377b87866f746452655d3c1c8ec40e5
Perelandra <luvyum4e@gmail.com>
2018-09-13 02:02:58 +0900

    Refactoring the sourcecode as a whole

--- sample-counter/src/main/java/com/perelandra/sample/counter/ui/main/CounterViewModel.kt
+++ sample-counter/src/main/java/com/perelandra/sample/counter/ui/main/CounterViewModel.kt
@@ -1,6 +1,7 @@
 package com.perelandra.sample.counter.ui.main
 
 import android.os.Parcelable
+import android.util.Log
 import com.perelandra.reactorviewmodel.ReactorViewModel
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -10,42 +11,50 @@ import java.util.concurrent.TimeUnit
 class CounterViewModel(initialState: State = State(value = 0, isLoading = false))
   : ReactorViewModel<CounterViewModel.Action, CounterViewModel.Mutation, CounterViewModel.State>(initialState) {
 
+  companion object {
+    private val TAG = CounterViewModel::class.java.simpleName
+  }
+
   sealed class Action {
-    object increase : Action()
-    object decrease : Action()
+    object Increase : Action()
+    object Decrease : Action()
   }
 
   sealed class Mutation {
-    object increaseValue : Mutation()
-    object decreaseValue : Mutation()
-    data class setLoading(val isLoading: Boolean) : Mutation()
+    object IncreaseValue : Mutation()
+    object DecreaseValue : Mutation()
+    data class SetLoading(val isLoading: Boolean) : Mutation()
   }
 
   @Parcelize
   data class State(
-    val name: String = CounterViewModel::class.java.name,
+    val name: String = TAG,
     val value: Int,
     val isLoading: Boolean
   ) : Parcelable
 
   override fun mutate(action: Action): Observable<Mutation> = when (action) {
-    is Action.increase -> Observable.concat(
-      Observable.just(Mutation.setLoading(true)),
-      Observable.just(Mutation.increaseValue).delay(500, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread()),
-      Observable.just(Mutation.setLoading(false)))
+    is Action.Increase -> Observable.concat(
+      Observable.just(Mutation.SetLoading(true)),
+      Observable.just(Mutation.IncreaseValue).delay(500, TimeUnit.MILLISECONDS),
+      Observable.just(Mutation.SetLoading(false)))
 
-    is Action.decrease -> Observable.concat(
-      Observable.just(Mutation.setLoading(true)),
-      Observable.just(Mutation.decreaseValue).delay(500, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread()),
-      Observable.just(Mutation.setLoading(false)))
+    is Action.Decrease -> Observable.concat(
+      Observable.just(Mutation.SetLoading(true)),
+      Observable.just(Mutation.DecreaseValue).delay(500, TimeUnit.MILLISECONDS),
+      Observable.just(Mutation.SetLoading(false)))
 
     else -> Observable.empty()
   }
 
   override fun reduce(state: State, mutation: CounterViewModel.Mutation): State = when (mutation) {
-    is Mutation.increaseValue -> state.copy(value = state.value.inc())
-    is Mutation.decreaseValue -> state.copy(value = state.value.dec())
-    is Mutation.setLoading -> state.copy(isLoading = mutation.isLoading)
+    is Mutation.IncreaseValue -> state.copy(value = state.value.inc())
+    is Mutation.DecreaseValue -> state.copy(value = state.value.dec())
+    is Mutation.SetLoading -> state.copy(isLoading = mutation.isLoading)
     else -> state
+  }
+
+  override fun transformState(state: Observable<State>): Observable<State> {
+    return super.transformState(state).observeOn(AndroidSchedulers.mainThread())
   }
 }
--- sample-githubsearch/src/main/java/com/perelandra/sample/githubsearch/ui/main/GithubSearchViewModel.kt
+++ sample-githubsearch/src/main/java/com/perelandra/sample/githubsearch/ui/main/GithubSearchViewModel.kt
@@ -1,40 +1,36 @@
 package com.perelandra.sample.githubsearch.ui.main
 
-import android.arch.lifecycle.Transformations.map
 import android.os.Parcelable
-import android.support.design.widget.Snackbar
 import android.util.Log
 import com.perelandra.reactorviewmodel.ReactorViewModel
 import io.reactivex.Observable
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.parcel.Parcelize
-import java.net.HttpURLConnection
 import java.net.URL
 
 class GithubSearchViewModel(initialState: State = State(query = "", repos = emptyList(), nextPage = 0, isLoadingNextPage = false)) :
   ReactorViewModel<GithubSearchViewModel.Action, GithubSearchViewModel.Mutation, GithubSearchViewModel.State>(initialState) {
 
   companion object {
-    private val TAG = this::class.java.simpleName
+    private val TAG = GithubSearchViewModel::class.java.simpleName
   }
 
   sealed class Action {
-    data class updateQuery(val query: String) : Action()
-    object loadNextPage : Action()
+    data class UpdateQuery(val query: String) : Action()
+    object LoadNextPage : Action()
   }
 
   sealed class Mutation {
-    data class setQuery(val query: String) : Mutation()
-    data class setRepos(val repos: List<String>, val nextPage: Int) : Mutation()
-    data class appendRepos(val repos: List<String>, val nextPage: Int) : Mutation()
-    data class setLoadingNextPage(val isLoadingNextPage: Boolean) : Mutation()
+    data class SetQuery(val query: String) : Mutation()
+    data class SetRepos(val repos: List<String>, val nextPage: Int) : Mutation()
+    data class AppendRepos(val repos: List<String>, val nextPage: Int) : Mutation()
+    data class SetLoadingNextPage(val isLoadingNextPage: Boolean) : Mutation()
   }
 
   @Parcelize
   data class State(
-    val name: String = GithubSearchViewModel::class.java.name,
+    val name: String = TAG,
     val query: String,
     val repos: List<String>,
     val nextPage: Int,
@@ -42,29 +38,44 @@ class GithubSearchViewModel(initialState: State = State(query = "", repos = empt
   ) : Parcelable
 
   override fun mutate(action: Action): Observable<Mutation> = when (action) {
-    is Action.updateQuery -> Observable.concat(
-      Observable.just(Mutation.setQuery(action.query)),
-      search(query = action.query, page = 1).map { Mutation.setRepos(emptyList(), it) })
+    is Action.UpdateQuery -> Observable.concat(
+      // 1) set current state's query
+      Observable.just(Mutation.SetQuery(action.query)),
+
+      // 2) call API and set repos
+      this.search(query = action.query, page = 1)
+        .map { Mutation.SetRepos(repos = it.first, nextPage = it.second) })
 
     else -> Observable.empty()
   }
 
   override fun reduce(state: State, mutation: GithubSearchViewModel.Mutation): State = when (mutation) {
-    is Mutation.setQuery -> state.copy(query = mutation.query)
-    is Mutation.setRepos -> state.copy(repos = mutation.repos, nextPage = mutation.nextPage)
+    is Mutation.SetQuery -> state.copy(query = mutation.query)
+    is Mutation.SetRepos -> state.copy(repos = mutation.repos, nextPage = mutation.nextPage)
     else -> state
+  }
+
+  override fun transformMutation(mutation: Observable<Mutation>): Observable<Mutation> {
+    return super.transformMutation(mutation).observeOn(Schedulers.io())
+  }
+
+  override fun transformState(state: Observable<State>): Observable<State> {
+    return super.transformState(state).observeOn(AndroidSchedulers.mainThread())
   }
 
   private fun url(query: String, page: Int): URL =
     URL("https://api.github.com/search/repositories?q=$query&page=$page")
 
-  private fun search(query: String, page: Int): Observable<Int> {
+  private fun search(query: String, page: Int): Observable<Pair<List<String>, Int>> {
+    var emptyResult = Pair<List<String>, Int>(emptyList(), 0)
     val url = url(query = query, page = page)
-    return Observable.just(url.openConnection())
-      .subscribeOn(Schedulers.io())
-      .doOnError { Log.i(TAG, "GithubSearchViewModel :: search : ${it.printStackTrace()}") }
-      .flatMap { Observable.just((it as HttpURLConnection).responseCode) }
 
+    return Observable.just(url.openConnection())
+      .map { emptyResult }
+      .doOnError { Log.i(TAG, "GithubSearchViewModel :: search : ${it.printStackTrace()}") }
+      .onErrorReturn { emptyResult }
+
+//      .flatMap { Observable.just((it as HttpURLConnection).responseCode) }
 //      .subscribe {
 //        Log.i(TAG, "GithubSearchViewModel :: search :: responseCode : $it")
 //      }
