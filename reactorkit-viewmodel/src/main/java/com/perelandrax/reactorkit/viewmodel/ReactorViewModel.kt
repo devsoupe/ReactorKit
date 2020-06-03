21d01b5f3c595f224c291634b83802c83a78ddfb
perelandrax <luvyum4e@gmail.com>
2020-06-03 11:33:05 +0900

    Update reactorViewModel

--- reactorkit-viewmodel/src/main/java/com/perelandrax/reactorkit/viewmodel/ReactorViewModel.kt
+++ reactorkit-viewmodel/src/main/java/com/perelandrax/reactorkit/viewmodel/ReactorViewModel.kt
@@ -1,3 +1,12 @@
 package com.perelandrax.reactorkit.viewmodel
 
-class ReactorViewModel
+import androidx.lifecycle.ViewModel
+import com.perelandrax.reactorkit.Reactor
+
+abstract class ReactorViewModel<Action, Mutation, State> : Reactor<Action, Mutation, State>, ViewModel() {
+
+  override fun onCleared() {
+    super.onCleared()
+    clear()
+  }
+}
